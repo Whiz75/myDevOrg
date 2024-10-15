26174f224867fb4143df8f26e35ceb61fc3326a3
Shane Khotsoka <ShaneKhotsoka@capitecbank.co.za>
2024-10-15 09:02:31 +0200

    Updated parentComponent

--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,8 +1,32 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+//import batchAccountTemplate from 'c/batchAccount/batchAccount.html';
+//import confirmationAndUpdateTemplate from 'c/confirmationAndUpdateScreen/confirmationAndUpdateScreen.html';
 
 export default class ParentComponent extends LightningElement {
+
+    @api accounts = [];
+    @api selectedAccounts = [];
+    @track isShowing = true;
+
     handleAccountsConfirmation(event) {
         const confirmationComponent = this.template.querySelector('c-confirmation-and-update-screen');
         confirmationComponent.handleAccountsConfirmation(event.detail.accounts);
     }
+    
+
+    // isBatchAccountScreen = true;  // Default to show batchAccount
+    // isConfirmationAndUpdateScreen = false; // Hidden initially
+
+    // // Method to handle the 'Next' button click
+    // handleNext() {
+    //     this.isBatchAccountScreen = false;
+    //     this.isConfirmationAndUpdateScreen = true;
+    // }
+
+    // // Method to handle the 'Back' button click
+    // handleBack() {
+    //     this.isBatchAccountScreen = true;
+    //     this.isConfirmationAndUpdateScreen = false;
+    // }
+    
 }
